7d3f0d0d403f07388ea3584320a9e4023101ebe6
Erivaldo-Montes <erivaldomontez@gmail.com>
2022-07-12 18:54:13 -0300

    fix responsive mode

--- src/pages/Event.tsx
+++ src/pages/Event.tsx
@@ -8,7 +8,7 @@ import { Footer } from "../components/footer/footer"
 export function Event() {
   const { slug } = useParams<{ slug: string }>()
   return (
-    <div className="flex flex-col max-h-screen md:overflow-y-auto overflow-y-scroll scrollbar-thin scrollbar-thumb-gray-500 scrollbar-track-transparent">
+    <div className="flex flex-col max-h-screen overflow-y-auto md:overflow-y-scroll scrollbar-thin scrollbar-thumb-gray-500 scrollbar-track-transparent">
       <Header />
       <main className="flex flex-1 max-w-full">
         {slug ? <Video lessonSlug={slug} /> : <EventLoading />}
