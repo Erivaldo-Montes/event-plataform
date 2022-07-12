304bac6a3ec12f17a23c33e5f9cbfffe3b325577
Erivaldo-Montes <erivaldomontez@gmail.com>
2022-07-12 19:12:31 -0300

    fix responsive mode

--- src/components/event/header/Header.tsx
+++ src/components/event/header/Header.tsx
@@ -3,7 +3,7 @@ import { Menu } from "../menu/menu"
 
 export function Header() {
   return (
-    <header className="bg-gray-700 max-h-[10vh] md:h-auto relative py-5 px-6 md:static flex items-center justify-between md:justify-center border-b border-gray-600">
+    <header className="bg-gray-700 max-h-[10%] md:h-auto relative py-5 px-6 md:static flex items-center justify-between md:justify-center border-b border-gray-600">
       <Logo />
       <Menu />
     </header>
