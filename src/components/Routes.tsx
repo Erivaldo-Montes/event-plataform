5dec516715800f08b6af1c80b293e5eb67ed31cb
Erivaldo-Montes <erivaldomontez@gmail.com>
2022-07-11 22:29:07 -0300

    add not found

--- src/components/Routes.tsx
+++ src/components/Routes.tsx
@@ -1,14 +1,16 @@
 import { Route, Routes } from "react-router-dom";
-import {Event} from "../pages/Event";
-import {Subscribe} from "../pages/Subscribe"
+import { Event } from "../pages/Event";
+import { Subscribe } from "../pages/Subscribe"
+import { NotFound } from "../components/notFound/notFound"
 
 // gerenciador de rotas
-export function Router(){
+export function Router() {
   return (
     <Routes>
-      <Route path="/" element={<Subscribe/>}/>
-      <Route path="/event" element={<Event/>}/>
-      <Route path="/event/lesson/:slug" element={<Event/>}/>
-    </Routes>   
+      <Route path="/" element={<Subscribe />} />
+      <Route path="/event" element={<Event />} />
+      <Route path="/event/lesson/:slug" element={<Event />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
   )
 }
--- src/components/loadingEventPage/teacherLoading.tsx
+++ src/components/loadingEventPage/teacherLoading.tsx
@@ -1,5 +1,4 @@
 export function TeacherLoading() {
-  console.log("ok")
   return (
     <div className="h-14 w-64 mt-6 animate-waintingCharge rounded bg-gradient-to-r from-gray-900 via-gray-700 to-gray-900">
     </div>
